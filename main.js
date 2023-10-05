e43ecf61e5edb14876674d3780905c13a0a46144
wsfmaw15 <127457500+wsfmaw15@users.noreply.github.com>
2023-10-05 20:29:16 -0300

    Add files via upload
    051023

---
+++ main.js
@@ -0,0 +1,53 @@
+function TocaSomPom () {document.querySelector('#som_tecla_pom').play();}
+
+document.querySelector('.tecla_pom').onclick = TocaSomPom;
+
+function TocaSomClap () {document.querySelector('#som_tecla_clap').play();}
+
+document.querySelector('.tecla_clap').onclick = TocaSomClap;
+
+function TocaSomTim () {document.querySelector('#som_tecla_tim').play();}
+
+document.querySelector('.tecla_tim').onclick = TocaSomTim;
+
+function TocaSomPuff () {document.querySelector('#som_tecla_puff').play();}
+
+document.querySelector('.tecla_puff').onclick = TocaSomPuff;
+
+
+function TocaSomSplash () {document.querySelector('#som_tecla_splash').play();}
+
+document.querySelector('.tecla_splash').onclick = TocaSomSplash;
+
+function TocaSomToim () {document.querySelector('#som_tecla_toim').play();}
+
+document.querySelector('.tecla_toim').onclick = TocaSomToim;
+
+
+function TocaSomPsh () {document.querySelector('#som_tecla_psh').play();}
+
+document.querySelector('.tecla_psh').onclick = TocaSomPsh;
+
+
+function TocaSomTic () {document.querySelector('#som_tecla_tic').play();}
+
+document.querySelector('.tecla_tic').onclick = TocaSomTic;
+
+function TocaSomTom () {document.querySelector('#som_tecla_tim').play();}
+
+document.querySelector('.tecla_tom').onclick = TocaSomTom;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
